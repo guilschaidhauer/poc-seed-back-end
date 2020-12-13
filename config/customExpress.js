ba9340083d4afaec35ca937683af068e6b3fbf7f
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2020-12-13 14:20:49 -0300

    Add cors support

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 module.exports = () => {
     const app = express();
 
+    app.use(cors());
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
