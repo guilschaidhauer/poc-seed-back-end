95a0ab038bd1eeacb8f329464818d015ec5724d2
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2021-12-11 16:11:11 -0300

    Add users table

--- infraestructure/tables.js
+++ infraestructure/tables.js
@@ -4,11 +4,24 @@ class Tables {
         console.log("tables creation was called");
 
         this.connection = connection;
+        this.createUsersTable();
         this.createBooksTable();
     }
 
+    createUsersTable() {
+        const sql = 'CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT, firstName varchar(30) NOT NULL, lastName varchar(40) NOT NULL, email varchar(100) NOT NULL, password CHAR(60) BINARY NOT NULL, PRIMARY KEY(id))';
+
+        this.connection.query(sql, (error => {
+            if(error) {
+                console.log(error);
+            } else {
+                console.log("Users table created successfully")
+            }
+        }));
+    }
+
     createBooksTable() {
-        const sql = 'CREATE TABLE IF NOT EXISTS books (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, description text, price int NOT NULL, PRIMARY KEY(id))';
+        const sql = 'CREATE TABLE IF NOT EXISTS books (id int NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, description text, price int NOT NULL, ownerId int NOT NULL, PRIMARY KEY(id), FOREIGN KEY (ownerId) REFERENCES users(id))';
 
         this.connection.query(sql, (error => {
             if(error) {
